21b62d1091cef86051b018a959c0e86e5bad8588
isleeden <nkoreanarmy@gmail.com>
2021-02-26 22:43:34 +0500

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const averStrLength = str.length / 2,
+        bracketsLength = bracketsConfig.length;
+    if (str.length % 2 != 0) {
+        return false;
+    }
+    for (let i = 0; i < averStrLength; i++) {
+        for (let j = 0; j < bracketsLength; j++) {
+            let bracketsType = bracketsConfig[j][0] + bracketsConfig[j][1];
+            while (str.indexOf(bracketsType) >= 0) {
+                str = str.replace(bracketsType, "");
+            }
+        }
+    }
+    return str == "" ? true : false;
+};
